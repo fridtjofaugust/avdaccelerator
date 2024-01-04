b5cf1a8b087931028decb9534017a2bcc20edb7a
Fridtjof August Barfod <fridtjof.barfod@innofactor.com>
2024-01-04 15:35:17 +0100

    updaate

--- workload/terraform/greenfield/AADscenario/avd.tf
+++ workload/terraform/greenfield/AADscenario/avd.tf
@@ -36,14 +36,14 @@ data "azurerm_role_definition" "power_role" {
 }
 
 data "azuread_service_principal" "spn" {
-  application_id = "9cdead84-a844-4324-93f2-b2e6bb768d07"
+  client_id = "9cdead84-a844-4324-93f2-b2e6bb768d07"
 }
 
 resource "azurerm_role_assignment" "power" {
   name                             = random_uuid.example.result
   scope                            = azurerm_resource_group.rg.id
   role_definition_id               = data.azurerm_role_definition.power_role.role_definition_id
-  principal_id                     = data.azuread_service_principal.spn.application_id
+  principal_id                     = data.azuread_service_principal.spn.client_id
   skip_service_principal_aad_check = true
   depends_on                       = [data.azurerm_role_definition.power_role]
 }
